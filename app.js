7dac18d68bfc9d72fa11d6ac7e0f6cd2c55559cb
sophearith saing <sophearithsaing123@gmail.com>
2020-01-08 16:16:55 +0700

    backend only and change path

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/", express.static(path.join(__dirname, "angular")));
+// app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
